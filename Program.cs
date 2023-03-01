e7f50aaa2199fd424803c873ade5723a02aa8d29
gentleman-8 <roc.no2004@gmail.com>
2023-03-01 19:39:31 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace user1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {555555555555555555555555555555555555555
+                888888888888888888888888888888888888888888888
+                77777777777777777777777777777777777777777777777777777
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
